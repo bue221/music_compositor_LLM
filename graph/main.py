afc922347930d7651b3d64718f29e13b58a43134
Andres Plaza (@bue221) <camiloplaza3@gmail.com>
2025-04-20 11:39:42 -0500

    feat(graph): add main workflow and configuration files

---
+++ graph/main.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, END
+
+from states.main import MusicState
+from graph.nodes import (
+    melody_generator,
+    harmony_generator,
+    rhythm_generator,
+    style_adapter,
+    midi_converter,
+)
+
+workflow = StateGraph(MusicState)
+
+workflow.add_node("melody_generator", melody_generator)
+workflow.add_node("harmony_creator", harmony_generator)
+workflow.add_node("rhythm_analyzer", rhythm_generator)
+workflow.add_node("style_adapter", style_adapter)
+workflow.add_node("midi_converter", midi_converter)
+
+workflow.set_entry_point("melody_generator")
+
+workflow.add_edge("melody_generator", "harmony_creator")
+workflow.add_edge("harmony_creator", "rhythm_analyzer")
+workflow.add_edge("rhythm_analyzer", "style_adapter")
+workflow.add_edge("style_adapter", "midi_converter")
+workflow.add_edge("midi_converter", END)
+
+app = workflow.compile()
